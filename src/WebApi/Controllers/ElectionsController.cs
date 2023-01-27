3c3f0dc225dbf9244385a98d84b530de2842b389
Nathan Hall <nathan@whatshark.com>
2023-01-27 17:47:49 +0000

    Add project files.

---
+++ src/WebApi/Controllers/ElectionsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Muk.Election.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public sealed class ElectionsController : ControllerBase
+{
+}
